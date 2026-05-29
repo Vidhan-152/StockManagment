2159807a84d560c440307a3cb916648f08d967c8
Vidhan 😎 <73152285+Vidhan-152@users.noreply.github.com>
2026-05-29 23:43:11 +0530

    auth

--- Backend/app/routes/auth.py
+++ Backend/app/routes/auth.py
@@ -1,4 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from schemas.auth import SignupRequest
+from db import get_db_connection
+from utils.security import hash_password
 
 # every router will start from /auth
 router = APIRouter(
@@ -6,6 +9,33 @@ router = APIRouter(
     tags = ["Auth"]
 )
 
-@router.get("/test")
-def test_auth():
-    return {"message" : "Auth is working"}
+@router.post("/signup")
+def signup(user: SignupRequest):
+    connection = get_db_connection()
+
+    if connection is None:
+        raise HTTPException(status_code = 500, detail = "Database connection failed")
+    
+    cursor = connection.cursor(dictionary=True)
+
+    cursor.execute(
+        "Select id from users where email = %s",
+        (user.email,)
+    )
+
+    existing_user = cursor.fetchone()
+
+    if existing_user:
+        cursor.close()
+        connection.close()
+        raise HTTPException(status_code = 400, detail="Email already registered")
+    
+    password_hash = hash_password(user.password)
+
+    cursor.execute(
+        """
+        insert into users(name, email, password_hash)
+        values (%s, %s, %s)
+        """,
+
+    )
